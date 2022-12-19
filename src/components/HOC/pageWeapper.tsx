3e2611d42f4f777abad520f73ac1b36cca06438c
Djibril-Mugisho <mugishodjibril2004@gmail>
2022-12-19 15:23:02 +0300

    fix padding issues

--- src/components/HOC/pageWeapper.tsx
+++ src/components/HOC/pageWeapper.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 const PageWeapper: React.FC<Props> = ({ children }) => {
-    return <div className="flex flex-col tablet:flex-row justify-center items-center gap-5 w-full h-full mt-32 relative px-5 flex-shrink ">
+    return <div className="flex flex-col tablet:flex-row justify-center items-center gap-5 w-full h-full mt-32 relative px-2 sm:px-5 flex-shrink ">
         {children}
     </div>;
 };
